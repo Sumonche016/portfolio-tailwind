3680106dab55291c835c52c1bf6a6440807cd14a
Sumonche016 <sumonche016@gmail.com>
2022-02-05 19:41:11 +0600

    hiii

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,5 @@
+// one page
+
 $(document).ready(function() {
     $('#ripples').ripples({
         resolution: 512,
@@ -7,12 +9,13 @@ $(document).ready(function() {
     });
 })
 
-// one page
+
 
 $('#nav').onePageNav();
 
 
 // nav 
+
 window.addEventListener('scroll', function() {
 
     let header = document.getElementById('header');
@@ -28,3 +31,20 @@ window.addEventListener('scroll', function() {
 //         section: ".example-classname",
 //     });
 // });
+
+
+
+//  this is for navbtn
+
+const menuList = document.querySelector('.menu-list');
+var cancel = document.querySelector('.cancel-btn');
+var menuBtn = document.querySelector('.menu-btn');
+
+menuBtn.onclick = () => {
+    menuList.classList.add('right-0');
+
+}
+cancel.onclick = () => {
+    menuList.classList.remove('right-0');
+
+}
